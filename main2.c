18b5ce701202029e277ef6164c44901539e57a5d
PajaS22 <49537767+PajaS22@users.noreply.github.com>
2021-04-20 12:21:10 +0200

    main init

---
+++ main2.c
@@ -0,0 +1,22 @@
+#define _POSIX_C_SOURCE 200112L
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <time.h>
+#include <unistd.h>
+
+#include "mzapo_parlcd.h"
+#include "mzapo_phys.h"
+#include "mzapo_regs.h"
+
+int main(int argc, char *argv[])
+{
+  printf("Hello world\n");
+
+  sleep(4);
+
+  printf("Goodbye world\n");
+
+  return 0;
+}
