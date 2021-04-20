f6b2912be59838ec8e318255d012a6e068b4990d
PajaS22 <49537767+PajaS22@users.noreply.github.com>
2021-04-20 11:28:18 +0200

    main init

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+#include <pthread.h>
+
+int main(int argc, char *argv[]){
+  int ret = EXIT_SUCCESS;
+  
+  return ret
+}
