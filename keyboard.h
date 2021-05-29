25e435580866358f89aa81c4dfc2b9885a6eb570
simii23 <simon.lehky7@gmail.com>
2021-05-29 15:50:22 +0200

    Comments added to keyboard files

--- keyboard.h
+++ keyboard.h
@@ -1,6 +1,15 @@
 #ifndef KEYBOARD_H
 #define KEYBOARD_H
 
+#define KEYBOARD_TIMEOUT 100
+
+/* 
+* wait KEYBOARD_TIMEOUT time for character from stdin
+* KEYBOARD_TIMEOUT is defined in keyboard.c
+* returns 1 if character has been read
+* returns 0 if no character has been read
+* returns -1 if error occured
+ */
 int keyboard_getc_timeout(unsigned char *c);
 
 #endif
