1433c73db6e27e44653c3e97a9b79ae3e94e2e18
simii23 <simon.lehky7@gmail.com>
2021-05-29 16:01:00 +0200

    Added comments to ball files

--- ball.c
+++ ball.c
@@ -1,16 +1,15 @@
-// pingpong ball
 #include "ball.h"
 
-#define POW2(x) x *x
+#define POW2(x) x*x
 
-void init_ball(Ball *b, Position pos, Velocity velocity, ushort color, int radius) {
+void init_ball(Ball *b, Position pos, Velocity velocity, pixel color, int radius) {
     b->color = color;
     b->radius = radius;
     b->velocity = velocity;
     b->pos = pos;
 }
 
-void draw_ball(Position pos, int radius, ushort color, ushort *frame_buff) {
+void draw_ball(Position pos, int radius, pixel color, pixel *frame_buff) {
     int first_x;
     for (int y = -radius; y <= radius; ++y) {
         first_x = round(sqrt(POW2(radius) - POW2(y)));
@@ -20,20 +19,19 @@ void draw_ball(Position pos, int radius, ushort color, ushort *frame_buff) {
     }
 }
 
-void update_ball(Position pos, Position old_pos, int radius, int old_radius, ushort color, ushort *frame_buff) {
-    draw_ball(old_pos, old_radius, BACKGROUND_COLOR, frame_buff);  // delete old from frame buffer
-    draw_ball(pos, radius, color, frame_buff);                     // draw new ball
+void update_ball(Position pos, Position old_pos, int radius, int old_radius, pixel color, pixel *frame_buff) {
+    draw_ball(old_pos, old_radius, BACKGROUND_COLOR, frame_buff);  // deleting the old ball from the frame buffer
+    draw_ball(pos, radius, color, frame_buff);                     // drawing a new ball
 }
 
 Position new_position(Ball *ball) {
-    return (Position){.X = ball->pos.X + ball->velocity.X, .Y = ball->pos.Y + ball->velocity.Y};
+    return (Position){ .X = ball->pos.X + ball->velocity.X, .Y = ball->pos.Y + ball->velocity.Y };
 }
 
 bool paddle_touch(float ball_y, float paddle_y, int paddle_height, int radius) {
     return (ball_y + radius >= paddle_y && ball_y - radius <= paddle_y + paddle_height);
 }
 
-// returns double between -1 and 1
 double rebound_angle(float ball_y, float paddle_y, int paddle_height) {
     float mid = paddle_y + (paddle_height / 2);
     double ret = (2 * (ball_y - mid) / paddle_height);
--- ball.h
+++ ball.h
@@ -10,14 +10,31 @@ typedef struct {
     Position pos;
     int radius;
     Velocity velocity;
-    ushort color;
+    pixel color;
 } Ball;
 
-void init_ball(Ball *b, Position pos, Velocity velocity, ushort color, int radius);
-void draw_ball(Position pos, int radius, ushort color, ushort *frame_buff);
-void update_ball(Position pos, Position old_pos, int radius, int old_radius, ushort color, ushort *frame_buff);
+/* initializes all data for the ball */
+void init_ball(Ball *b, Position pos, Velocity velocity, pixel color, int radius);
+
+/* draw ball in its actual position in the frame buffer */
+void draw_ball(Position pos, int radius, pixel color, pixel *frame_buff);
+
+/*
+ * draws the old ball in the color of the background
+ * than draws the new ball in the color argument in the new position
+ */
+void update_ball(Position pos, Position old_pos, int radius, int old_radius, pixel color, pixel *frame_buff);
+
+/* returns the position + velocity of the ball */
 Position new_position(Ball *ball);
+
+/* controls whether the ball is in given paddle's range on y-axis */
 bool paddle_touch(float ball_y, float paddle_y, int paddle_height, int radius);
+
+/*  
+ * computes the ball's rebound angle of the paddle's curved surface
+ * returns double between -1 and 1
+ */
 double rebound_angle(float ball_y, float paddle_y, int paddle_height);
 
 #endif
