a6d9b6ee79cc7efb067d1b5e4606d4068cf63103
simii23 <simon.lehky7@gmail.com>
2021-05-29 16:12:17 +0200

    Added comments to paddle files

--- paddle.c
+++ paddle.c
@@ -1,15 +1,14 @@
-// pingpong paddle
 #include "paddle.h"
 #include "LCD_output.h"
 
-void init_paddle(Paddle *p, Position pos, ushort color) {
+void init_paddle(Paddle *p, Position pos, pixel color) {
     p->color = color;
     p->height = PADDLE_LENGTH;
     p->width = PADDLE_WIDTH;
     p->pos = pos;
 }
 
-void draw_paddle(Position position, int paddle_width, int paddle_length, ushort color, ushort *frame_buff) {
+void draw_paddle(Position position, int paddle_width, int paddle_length, pixel color, pixel *frame_buff) {
     int x = position.X;
     int y = position.Y;
     for (int i = 0; i < paddle_length; ++i) {
@@ -19,7 +18,7 @@ void draw_paddle(Position position, int paddle_width, int paddle_length, ushort 
     }
 }
 
-void update_paddle(Position position, Position old_position, int width, int length, int old_length, ushort color, ushort *frame_buff) {
+void update_paddle(Position position, Position old_position, int width, int length, int old_length, pixel color, pixel *frame_buff) {
     draw_paddle(old_position, width, old_length, BACKGROUND_COLOR, frame_buff);
     draw_paddle(position, width, length, color, frame_buff);
 }
--- paddle.h
+++ paddle.h
@@ -10,12 +10,25 @@ typedef struct {
     Position pos;
     int width;
     int height;
-    ushort color;
+    pixel color;
 } Paddle;
 
-void init_paddle(Paddle *p, Position pos, ushort color);
-void draw_paddle(Position position, int paddle_width, int paddle_length, ushort color, ushort *frame_buff);
-void update_paddle(Position position, Position old_position, int width, int length, int old_length, ushort color, ushort *frame_buff);
+/* initializes all data for the paddle */
+void init_paddle(Paddle *p, Position pos, pixel color);
+
+/* draw paddle in its actual position in the frame buffer */
+void draw_paddle(Position position, int paddle_width, int paddle_length, pixel color, pixel *frame_buff);
+
+/*
+ * draws the old paddle in the color of the background
+ * than draws the new paddle in the color argument in the new position
+ */
+void update_paddle(Position position, Position old_position, int width, int length, int old_length, pixel color, pixel *frame_buff);
+
+/*
+ * changes the y-coordinate of position depending on the move integer
+ * controls if the new position is legal (in the display borders)
+ */
 void move_paddle(Paddle *p, int move);
 
 #endif
